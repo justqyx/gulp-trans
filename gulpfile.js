c0ae35452fd541499b93fcfb6cd933a885e4a76f
yuanxin.qiu <hzuqiuyuanxin@gmail.com>
2015-09-30 11:43:13 +0800

    Format Code

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var trans = require('./');
 
 gulp.task('default', function() {
-	gulp.src('data/src/*.md')
-		.pipe(trans())
-		.pipe(gulp.dest('data/dist'));
-		 
-		var open = require("open");
-		open("./data/dist/sample.html");
+    gulp.src('data/src/*.md')
+        .pipe(trans())
+        .pipe(gulp.dest('data/dist'));
+
+    var open = require("open");
+    open("./data/dist/sample.html");
 });
--- test.js
+++ test.js
@@ -3,20 +3,20 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var markdown = require('./');
 
-it('should compile Markdown to HTML', function (cb) {
-	var stream = markdown();
+it('should compile Markdown to HTML', function(cb) {
+    var stream = markdown();
 
-	stream.once('data', function (file) {
-		assert.equal(file.relative, 'fixture.html');
-		// assert.equal(file.contents.toString(), '<p><em>foo</em></p>\n');
-	});
+    stream.once('data', function(file) {
+        assert.equal(file.relative, 'fixture.html');
+        // assert.equal(file.contents.toString(), '<p><em>foo</em></p>\n');
+    });
 
-	stream.on('end', cb);
+    stream.on('end', cb);
 
-	stream.write(new gutil.File({
-		path: 'fixture.md',
-		contents: new Buffer('*foo*')
-	}));
+    stream.write(new gutil.File({
+        path: 'fixture.md',
+        contents: new Buffer('*foo*')
+    }));
 
-	stream.end();
+    stream.end();
 });
