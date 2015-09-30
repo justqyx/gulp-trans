051c0f37d09d6cfd88f8c2d4e54f292a9cd0c323
yuanxin.qiu <hzuqiuyuanxin@gmail.com>
2015-09-30 14:13:35 +0800

    remove comment code

--- index.js
+++ index.js
@@ -45,37 +45,5 @@ module.exports = function(options) {
                 cb(null, file);
             });
         });
-
-        // var rs = fs.createReadStream('./template.html', {
-        //     encoding: 'utf-8',
-        //     bufferSize: 11
-        // });
-
-        // var bufferHelper = new BufferHelper();
-
-        // rs.on("data", function(trunk) {
-        //     bufferHelper.concat(trunk);
-        // });
-
-        // rs.on("end", function() {
-        //     var source = bufferHelper.toBuffer().toString();
-        //     var template = Handlebars.compile(source);
-
-        //     marked(file.contents.toString(), options, function(err, content) {
-        //         if (err) {
-        //             cb(new gutil.PluginError('gulp-markdown', err, {
-        //                 fileName: file.path
-        //             }));
-        //             return;
-        //         }
-
-        //         file.contents = new Buffer(template({ parse_markdown: content }));
-        //         file.path = gutil.replaceExtension(file.path, '.html');
-
-        //         cb(null, file);
-        //     });
-
-        // });
-
     });
 };
